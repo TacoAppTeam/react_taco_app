34d466dd0b2ac637849d6f1442c482dc99ff55a9
root <root@localhost.localdomain>
2017-04-28 13:02:38 -0500

    (vws-002) Second commit! Versions

--- hug/taco.py
+++ hug/taco.py
@@ -2,11 +2,23 @@
 import hug
 
 
-@hug.get()
-def taco():
+@hug.get(versions=2, examples="style=fish")
+def taco(style: hug.types.text):
+    """
+    Show the tacos endpoint.
+    
+    Here are the tacos.
+    """
+    return {"tacos": "are %s" % style}
+
+@hug.get(versions=1, examples="style=1")
+def taco(style: int):
     """
     Show the tacos endpoint.
 
     Here are the tacos.
     """
+    # TODO 
     return {"tacos": "are gooood"}
+
+# TODO Add
