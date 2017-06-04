b0e25c8bc4886341cee6ca7a402643c6192e3309
Cody Abney <cody.abney@viasat.com>
2017-06-04 15:19:50 -0500

    (restructuring_of_app) Added stuff that looks important for the app to run.  Added button to make api calls and added functions that are a work in progress.

--- vws_taco_reactapp/src/App.js
+++ vws_taco_reactapp/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'whatwg-fetch';
+import Request from 'react-http-request';
 
 class App extends Component {
   render() {
@@ -13,9 +15,52 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={callApiFetch}>Click Me!</button>
       </div>
     );
   }
+}
+
+
+var checkStatus = function(response) {
+  console.log(response);
+  if (response.status >= 200 && response.status < 300) {
+    console.log(response);
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    console.log(error.response);
+  }
+}
+
+
+// TODO: Not working.  Trying to call hug api using whatwg-fetch
+var callApiFetch = function () {
+  console.log('callApi happened');
+
+  fetch('http://192.168.56.101:8000/v1/event?id=1',
+    {
+      mode: 'no-cors',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(checkStatus)
+}
+
+
+// TODO: Not working.  Trying to call hug api using react-http-request
+var callApiRequest = function () {
+  return (
+    <Request
+      url='http://192.168.56.101:8000/v1/event?id=1'
+      method='get'
+      accept='application/json'
+      verbose={true}
+    >
+    </Request>
+  );
 }
 
 export default App;
