0a96b467128cffd0bf18b9db2b00f53238e785e7
Cody Abney <cody.abney@viasat.com>
2017-07-14 11:43:50 -0500

    Added Title component and updated Login component to use it

--- vws_taco_reactapp/src/Login.js
+++ vws_taco_reactapp/src/Login.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
+import Title from './Title'
 
 export default class Login extends Component {
   render() {
     return (
-      <div className="Login">
-        <h1>Taco Title</h1>
+      <div className="login">
+        <Title></Title>
         <input name="username" placeholder="Username" type="text"/>
         <br></br>
         <input name="password" placeholder="Password" type="password"/>
