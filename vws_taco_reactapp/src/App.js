0b4e999f3b65363be173d02b79e756411a191ad1
Josh Masterson <joshua.masterson@viasat.com>
2017-07-14 12:02:07 -0500

    dumber Josh

---
+++ vws_taco_reactapp/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
+import Home from './Home';
+import Login from './Login';
+import Title from './Title';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginToken: undefined,
+      handleSubmit: (e) => {
+        e.preventDefault();
+        this.setState({loginToken: "HHHJJJKKK"});
+      }
+    };
+  }
+
+  render() {
+    if(!this.state || !this.state.loginToken) {
+      return (
+        <Title></Title>
+        <Login handleSubmit={this.state.handleSubmit}/>
+        );
+    } else {
+      return (
+        <Home loginToken={this.state.loginToken} />
+      )
+    }
+  }
+}
+
+const Taco = () => <h1>Welcome to the Taco App.</h1>
+const NotFound = () => (<h1>404.. This page is not found!</h1>)
+export default App
