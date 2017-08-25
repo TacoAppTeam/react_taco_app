34ebebefa0803c2b5c004f6244956d42d728d781
jmasterson <jmasterson@viasat.com>
2017-08-25 12:58:20 -0500

    Added ingredientslist first pass

--- vws_taco_reactapp/src/OrderBuilder.js
+++ vws_taco_reactapp/src/OrderBuilder.js
@@ -3,10 +3,26 @@ import IngredientsList from './IngredientsList';
 import OrderContents from './OrderContents';
 
 export default class OrderBuilder extends Component {
+  constructor (props) {
+    super(props);
+
+    this.ingredients = [
+        'Egg',
+        'Bacon',
+        'Sausage',
+        'Cheese'
+      ]
+  }
+
+  handleAddTaco (taco) {
+    console.log('taco!');
+    // TODO set state of orderlist with new taco
+  }
+
   render() {
     return (
       <div>
-      <IngredientsList></IngredientsList>
+      <IngredientsList ingredients={this.ingredients} handleAddTaco={ this.handleAddTaco }></IngredientsList>
       <OrderContents></OrderContents>
       </div>
     );
