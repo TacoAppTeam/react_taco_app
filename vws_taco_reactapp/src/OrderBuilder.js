02a69d6cbf82f628f796c49bcd6933711a0cd999
jmasterson <jmasterson@viasat.com>
2017-08-25 11:57:10 -0500

    Initial commit PT 2

--- vws_taco_reactapp/src/OrderBuilder.js
+++ vws_taco_reactapp/src/OrderBuilder.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import IngredientsList from './IngredientsList';
+import OrderContents from './OrderContents';
 
 export default class OrderBuilder extends Component {
   render() {
     return (
-      <div>ORDER BUILDER</div>
+      <div>
+      <IngredientsList></IngredientsList>
+      <OrderContents></OrderContents>
+      </div>
     );
   }
 }
