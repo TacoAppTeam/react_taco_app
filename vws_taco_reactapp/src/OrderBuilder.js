45e4896a152345d6f306def3e2ee5c24fe923e66
Ross Hudgins <michael.hudgins@viasat.com>
2017-08-25 12:55:45 -0500

    added OrderContents component

--- vws_taco_reactapp/src/OrderBuilder.js
+++ vws_taco_reactapp/src/OrderBuilder.js
@@ -3,11 +3,20 @@ import IngredientsList from './IngredientsList';
 import OrderContents from './OrderContents';
 
 export default class OrderBuilder extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      orderList: [{orderId: 1, desc: 'Egg, Sausage', count: 3}, 
+      	{orderId: 2, desc: 'Egg, Potato', count: 2}
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
       <IngredientsList></IngredientsList>
-      <OrderContents></OrderContents>
+      <OrderContents orderList={this.state.orderList}></OrderContents>
       </div>
     );
   }
