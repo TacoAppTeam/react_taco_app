5e829e31c4915aff68719c6c17fdccdec4ac7d8c
Ross Hudgins <michael.hudgins@viasat.com>
2017-09-01 12:56:57 -0500

    (orderbuilder) ingredient list gets from db

--- vws_taco_reactapp/src/OrderBuilder.js
+++ vws_taco_reactapp/src/OrderBuilder.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import IngredientsList from './IngredientsList';
 import OrderContents from './OrderContents';
+import Request from 'react-http-request';
 
 export default class OrderBuilder extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      orderList: [{orderId: 1, desc: 'Egg, Sausage', count: 3}, 
+      orderList: [{orderId: 1, desc: 'Egg, Sausage', count: 3},
       	{orderId: 2, desc: 'Egg, Potato', count: 2}
       ]
     };
-    this.ingredients = [
-        'Egg',
-        'Bacon',
-        'Sausage',
-        'Cheese'
-      ]
   }
 
   handleAddTaco (taco) {
@@ -26,7 +21,40 @@ export default class OrderBuilder extends Component {
   render() {
     return (
       <div>
-      <IngredientsList ingredients={this.ingredients} handleAddTaco={ this.handleAddTaco }></IngredientsList>
+      <Request
+        url='http://192.168.56.101:8000/v1/ingredients'
+        method='get'
+        accept='application/json'
+        verbose={true}
+        mode='no-cors'
+      >
+        {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            } else {
+
+              let body = result && result.body;
+
+              let data = [];
+              for(var ingredientData of body) {
+                let ingredient = {};
+                ingredient.id = ingredientData.ingredient.id;
+                ingredient.name = ingredientData.ingredient.name;
+                ingredient.description = ingredientData.ingredient.description;
+                ingredient.price = ingredientData.ingredient.price;
+                data.push(ingredient);
+              }
+
+              return (
+                <IngredientsList ingredients={data} handleAddTaco={ this.handleAddTaco }></IngredientsList>
+              );
+
+            }
+          }
+        }
+      </Request>
+
       <OrderContents orderList={this.state.orderList}></OrderContents>
       </div>
     );
