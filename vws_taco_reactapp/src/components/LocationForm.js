bc033aeb653ca429bcbe3e1906b319a1271079d6
Cody Abney <cody.abney@viasat.com>
2018-04-29 13:10:09 -0500

    (location-mgmt) Added fields to LocationForm

--- vws_taco_reactapp/src/components/LocationForm.js
+++ vws_taco_reactapp/src/components/LocationForm.js
@@ -7,6 +7,12 @@ export default class LocationForm extends Component {
     this.state = {
       name: '',
       basePrice: '',
+      street_address: '',
+      city: '',
+      state: '',
+      zip: '',
+      phone_number: '',
+      hours: '',
     };
   }
 
@@ -33,15 +39,77 @@ export default class LocationForm extends Component {
             handleChange('name', val);
           }}
         />
+        <br />
+        <TextField
+          name="street_address"
+          placeholder="Street Address"
+          type="text"
+          value={this.state.street_address}
+          onChange={(e, idx, val) => {
+            handleChange('street_address', val);
+          }}
+        />
+        <br />
+        <TextField
+          name="city"
+          placeholder="City"
+          type="text"
+          value={this.state.city}
+          onChange={(e, idx, val) => {
+            handleChange('city', val);
+          }}
+        />
+        <br />
+        <TextField
+          name="state"
+          placeholder="State"
+          type="text"
+          value={this.state.state}
+          onChange={(e, idx, val) => {
+            handleChange('state', val);
+          }}
+        />
+        <br />
+        <TextField
+          name="zip"
+          placeholder="Zip Code"
+          type="text"
+          value={this.state.zip}
+          onChange={(e, idx, val) => {
+            handleChange('zip', val);
+          }}
+        />
+        <br />
+        <TextField
+          name="phone_number"
+          placeholder="Phone Number"
+          type="text"
+          value={this.state.phone_number}
+          onChange={(e, idx, val) => {
+            handleChange('phone_number', val);
+          }}
+        />
+        <br />
+        <TextField
+          name="hours"
+          placeholder="Hours"
+          type="text"
+          value={this.state.hours}
+          onChange={(e, idx, val) => {
+            handleChange('hours', val);
+          }}
+        />
+        <br />
         <TextField
           name="base_taco_price"
           placeholder="Base Taco Price"
           type="text"
           value={this.state.basePrice}
           onChange={(e, idx, val) => {
-            handleChange('name', val);
+            handleChange('base_taco_price', val);
           }}
         />
+        <br />
       </form>
     );
   }
