eec4ab5699b443a9942546e24e68967cdb937bb0
Josh Masterson <joshua.masterson@viasat.com>
2018-07-01 12:43:07 -0500

    testing

--- ecr-login.sh
+++ ecr-login.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-aws_login=$(aws ecr get-login --region us-west-2);
+aws_login=$(aws ecr get-login --region us-west-2 --no-include-email);
 echo "Login is ${aws_login}"
 # if echo "$aws_login" | grep -q -E '^docker login -u AWS -p \S{1092} -e none https://[0-9]{12}.dkr.ecr.\S+.amazonaws.com'; then $aws_login; fi
 $aws_login
