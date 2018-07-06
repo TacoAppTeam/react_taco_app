4180e6af280e74adefc1e9d1309f7e1b8fb68270
mhudgins <michael.hudgins@viasat.com>
2018-07-06 12:37:17 -0500

    add http before hostname...

--- vws_taco_reactapp/src/config.js
+++ vws_taco_reactapp/src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  api_hostname: process.env.API_HOSTNAME || 'http://localhost',
+  api_hostname: 'http://' + process.env.API_HOSTNAME || 'http://localhost',
   api_port: process.env.API_PORT || 8000
 };
